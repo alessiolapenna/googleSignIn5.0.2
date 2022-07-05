f5fc6c790e34703848648902ea6686a5c4013616
Alessio la Penna <alessio.lapenna@gmail.com>
2022-07-05 13:18:11 +0900

    path fix

--- Package.swift
+++ Package.swift
@@ -23,6 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name:
                         "PGoogleSignIn", path:
-                        "Resources/PGoogleSignIn.xcframework")
+                        "PGoogleSignIn.xcframework")
     ]
 )
